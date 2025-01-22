883fb929589ba9973d47ec775cc2ae5ada5b8d92
leandroW <leandro_antunes28@hotmail.com>
2025-01-22 05:57:52 -0300

    editando arquivo actions

---
+++ src/controladores/login.js
@@ -0,0 +1,42 @@
+const bcript = require("bcrypt");
+const knex = require("../conexao");
+const jwt = require("jsonwebtoken");
+const senhaSecreta = require("../senhaSecreta");
+
+const login = async (req, res) => {
+  const { email, senha } = req.body;
+
+  if (!email || !senha) {
+    return res.status(400).json({
+      mensagem: "Preencha os campos obrigatórios: email e senha",
+    });
+  }
+
+  try {
+    const buscarConta = await knex("conta_bancaria").where({ email }).first();
+
+    if (!buscarConta) {
+      return res.status(400).json({ mensagem: "email ou senha invalido" });
+    }
+    const senhaValida = await bcript.compare(senha, buscarConta.senha);
+
+    if (!senhaValida) {
+      return res.status(400).json({ mensagem: "email ou senha invalido" });
+    }
+
+    const token = jwt.sign({ id: buscarConta.id }, senhaSecreta, {
+      expiresIn: "8h",
+    });
+
+    const { senha: senhaUsuario, ...usuarioLogado } = buscarConta;
+    return res.status(200).json({ usuario: usuarioLogado, token });
+  } catch (erro) {
+    return res.status(500).json({ mensagem: "ocorreu um erro"});
+  }
+};
+
+
+
+module.exports = {
+  login,
+};
--- src/rotas.js
+++ src/rotas.js
@@ -4,18 +4,11 @@ const { criarContaBancaria } = require("./controladores/ContaBancaria");
 const { depositarDinheiro } = require("./controladores/Depositos");
 const { transferirDinheiro } = require("./controladores/tranferencia");
 
-const {
-  cadastrarUsuario,
-  login,
-  detalharPerfilUsuario,
-  atualizarPerfilUsuario,
-} = require("./controladores/usuarios");
+const { login } = require("./controladores/login");
 
 const rotas = express();
 
-rotas.post("/usuario", cadastrarUsuario);
 rotas.post("/conta", criarContaBancaria);
-
 
 rotas.post("/login", login);
 
@@ -23,11 +16,6 @@ rotas.use(verificarUsuarioLogado);
 
 rotas.post("/depositar", depositarDinheiro);
 rotas.post("/transferir", transferirDinheiro);
-
-
-rotas.get("/usuario", detalharPerfilUsuario);
-rotas.put("/usuario", atualizarPerfilUsuario);
-
 
 module.exports = rotas;
 
